5339d10e5c676ad858bb2e83b4fa67a4cc45e212
unknown <c2lbiz8@.(none)>
2015-03-27 11:29:37 +0530

    Google fonts added and static page text align to center

--- app/controllers/staticpages_controller.rb
+++ app/controllers/staticpages_controller.rb
@@ -1,5 +1,6 @@
 class StaticpagesController < ApplicationController
   def index
-  	@quotes = Quote.order("RANDOM()");
+  	# @quotes = Quote.order("RANDOM()");
+  	@quote = Quote.order("RANDOM()").last
   end
 end
